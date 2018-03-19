c780d657003e0a8b9598cc0e87850acabfab42b4
Yuliya Yasenetska <jasenetska@gmail.com>
2018-03-19 17:31:46 -0400

    career page buttons

--- barkdoc-fronend/src/components/publicpages/Careers.js
+++ barkdoc-fronend/src/components/publicpages/Careers.js
@@ -13,15 +13,22 @@ class Careers extends Component {
         </div>
         <div className="white">
         <div className="content">
+        <div className="margin-right-left">
           <div className="center grey-font pt-26 bold">Join BarkDoc</div>
           <hr className="grey-font length-11"/>
-          <h2 className="orange-font">About the Team</h2>
-          <p className="grey-font">Here at BarkDoc we truely care about your pets and assure they will get the most caring, smart and perfect Vet for their needs. If you’re looking to become apart of the BarkDoc team see what we have available below.</p>
+          <h2 className="orange-font pt-20">About the Team</h2>
+          <p className="grey-font pt-14">Here at BarkDoc we truely care about your pets and assure they will get the most caring, smart and perfect Vet for their needs. If you’re looking to become apart of the BarkDoc team see what we have available below.</p>
           <hr className="grey-font" />
-          <h2 className="grey-font bold">Veterenarian</h2>
+          <div className="row">
+            <div className="margin-20 column-job-name"><span className="grey-font bold  pt-14">Veterenarian </span><span className="light-grey-font bold font-9">New York, NY</span></div>
+            <button className="float-right column-button button-apply-now-grey white-font bold">APPLY NOW</button>
+          </div>
           <hr className="grey-font"/>
-          <h2 className="grey-font bold">Vet Tech</h2>
+          <div className="margin-20"><span className="grey-font bold  pt-14">Vet Tech </span><span className="light-grey-font bold font-9 ">New York, NY</span></div>
           <hr className="grey-font"/>
+
+
+        </div>
         </div>
         </div>
         <HaveQuestionsForm />
