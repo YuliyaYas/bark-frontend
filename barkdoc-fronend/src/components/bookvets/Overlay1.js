a73855aba438001aa2ba30e4632d6bb3f7c6cd10
Yuliya Yasenetska <jasenetska@gmail.com>
2018-03-30 13:41:31 -0400

    adding styling to overlay

--- barkdoc-fronend/src/components/bookvets/Overlay1.js
+++ barkdoc-fronend/src/components/bookvets/Overlay1.js
@@ -5,6 +5,20 @@ const Overlay1 = () => {
   return(
     <div className="overlay-popup">
       <img className="logo-white" src={require("./../../img/repeated/white-logo.png")} alt=""/>
+    <div>
+      <form>
+      <h1>Where Should We Swing By?</h1>
+      <p> Let us know your pet's location to continue </p>
+      <p>*My Pet's Location:</p>
+      <select className="styled-select grey-font bold">
+        <option value="location">Manhattan</option>
+        <option value="location">Bronx</option>
+        <option value="location">Queens</option>
+        <option value="location">Staten Island</option>
+      </select>
+      <button type="submit" className="orange white-font display-block bold continue-button">CONTINUE</button>
+      </form>
+    </div>
     </div>
   );
 };
