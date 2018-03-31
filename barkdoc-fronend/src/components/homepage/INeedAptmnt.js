fe2b6178d10304f3fc91cefffaa17dafaf233bad
dansbands <dansbands@hotmail.com>
2018-03-30 20:06:59 -0400

    some layout stuff

--- barkdoc-fronend/src/components/homepage/INeedAptmnt.js
+++ barkdoc-fronend/src/components/homepage/INeedAptmnt.js
@@ -5,15 +5,15 @@ class INeedAptmnt extends Component{
   render(){
     return(
       <div className="content">
-      <div className="font-40pt width-35 top-100 white-font bold">
+      <div className="font-40pt width-50 top-100 white-font bold">
         <div>Vet Appointments.</div>
         <div>At Your Home.</div>
         <div>Furrr Real.</div>
       </div>
-      <div className="width-35">
-      <div className="white-font font-16 float-left margin-top-20">I need an appointment for my</div>
+      <div className="width-45">
+      <div className="white-font font-16 float-left-desktop margin-top-20">I need an appointment for my</div>
       <form>
-      <div className="float-left ">
+      <div className="float-right">
       <select className="styled grey-font margin-top-16 bold">
         <option value="dog">Dog</option>
         <option value="cat">Cat</option>
