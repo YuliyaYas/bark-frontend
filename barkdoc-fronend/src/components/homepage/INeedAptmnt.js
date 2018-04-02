976a37552b4b879b3a19fc2c58d2ea34ac8f3284
dansbands <dansbands@hotmail.com>
2018-04-02 16:43:37 -0400

    orange arrow dropdown

--- barkdoc-fronend/src/components/homepage/INeedAptmnt.js
+++ barkdoc-fronend/src/components/homepage/INeedAptmnt.js
@@ -11,13 +11,15 @@ class INeedAptmnt extends Component{
         <div>Furrr Real.</div>
       </div>
       <div className="width-45">
-      <div className="white-font font-16 float-left-desktop margin-top-20">I need an appointment for my</div>
+      <div className="white-font font-16 float-left-desktop margin-top-20 padding-10">I need an appointment for my</div>
       <form>
-      <div className="float-right">
+      <div className="float-right select">
       <select className="styled grey-font margin-top-16 bold">
         <option value="dog">Dog</option>
         <option value="cat">Cat</option>
       </select>
+      <div className="select_arrow">
+      </div>
       </div>
       <Link to="/user/vets"><button type="submit" className="orange no-border white-font continue-button">CONTINUE</button></Link>
       </form>
