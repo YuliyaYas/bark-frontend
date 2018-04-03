b359efbdb889c5bd4758aeadc79e1091493c4ea8
Yuliya Yasenetska <jasenetska@gmail.com>
2018-04-03 00:20:49 -0400

    adding vet profile

--- barkdoc-fronend/src/components/vetprofile/VetProfile.js
+++ barkdoc-fronend/src/components/vetprofile/VetProfile.js
@@ -11,12 +11,13 @@ const VetProfile = () => {
         <Header />
         <h1 className="white-font bold content">Dr. Ryan U, VMD</h1>
         <h3 className="white-font content">Veterinariae Medicinae Doctor</h3>
-        <h3 className="white-font content">***** Verified Patient Reviews</h3>
+        <h3 className="white-font content">***** Verified Patient Reviews(6)</h3>
       </div>
 
       <div className="row white">
         <div className="content">
           <div className="column-70 grey-font">
+            <img className="circle-img" src={require("../../img/vet_profile.png")}/>
             <h2 className="bold">Services Dr. Ryan Offers:</h2>
               <div className="column-33">
                 <p>Routine exams</p>
@@ -32,13 +33,25 @@ const VetProfile = () => {
                 <p>Allergy testing</p>
                 <p>Heqrtworm testing</p>
               </div>
-              <div className="column-33">
+              <div className="column-34">
                 <p>Parasite testing</p>
                 <p>Tick testing</p>
                 <p>Urine Testing</p>
                 <p>Ultrasounds</p>
                 <p>Cold Laser Therapy</p>
               </div>
+              <hr />
+              <h2 className="bold">Education and Service</h2>
+              <p>
+              Medical School - Cornell University, Veterinariae Medicinae Doctor
+              </p>
+              <p>
+              Dr. Ryan loves to learn and grow as Veterinary Medicine evolves. Through the years he has taken extensive continuing education in Laser Surgery, Ultrasound,
+              Echocardiograms, Endoscopy and Cold Laser Therapy.
+              </p>
+              <hr />
+              <h2 className="bold">Verified Patient Reviews (6)</h2>
+
           </div>
         </div>
         <div className="column-30">
