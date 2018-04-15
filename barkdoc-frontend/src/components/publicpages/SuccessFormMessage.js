4138c9b86571eb0080e205afbb4a8fce89697d85
Yuliya Yasenetska <jasenetska@gmail.com>
2018-04-14 22:25:02 -0400

    success message on form submission

--- barkdoc-frontend/src/components/publicpages/SuccessFormMessage.js
+++ barkdoc-frontend/src/components/publicpages/SuccessFormMessage.js
@@ -3,7 +3,8 @@ import React from 'react';
 const SuccessFormMessage = () => {
   return(
     <div>
-      <p className="success-message" style={{color: 'green'}}>Thank you! Form was submitted successfully!</p>
+      <h1 className="success-message" style={{color: 'orange', textAlign: 'center', marginTop: '50px'}}>Thank you!</h1>
+      <h1 className="success-message" style={{color: 'orange', textAlign: 'center'}}>Form was submitted successfully.</h1>
     </div>
   );
 };
