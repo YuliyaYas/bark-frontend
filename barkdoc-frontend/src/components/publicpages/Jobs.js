2c78097f01d4b6bb3f0056199cdf6854bc887200
dansbands <dansbands@hotmail.com>
2018-04-27 15:41:05 -0400

    career click-in closer to mobile

--- barkdoc-frontend/src/components/publicpages/Jobs.js
+++ barkdoc-frontend/src/components/publicpages/Jobs.js
@@ -11,20 +11,21 @@ const Jobs = (props) => {
       {props.job_posts.length>0 ?
         <div className="content-job">
           <div className="margin-top-80px">
-            <span className="grey-font bold  pt-20">{props.job_posts[0].title} </span>
+            <span className="grey-font bold pt-20 title">{props.job_posts[0].title} </span>
+            <br className="mobile"></br>
             <span className="light-grey-font bold font-14 "> {props.job_posts[0].location}</span>
           </div>
           <h2 className="orange-font">About the Position</h2>
           <p>{props.job_posts[0].about}</p>
-          <h2 className="orange-font">Whar Youll Do: </h2>
-          <ul>{props.job_posts[0].responsibilities.split('•').map((st,i)=>{
+          <h2 className="orange-font">What You'll Do: </h2>
+          <ul className="job-description">{props.job_posts[0].responsibilities.split('•').map((st,i)=>{
           if (st){
             return <li key={i}>{st}</li>
             }
           }) }</ul>
           <h2 className="orange-font">Qualifications: </h2>
 
-          <ul>{props.job_posts[0].qualifications.split('•').map((st,i)=> {
+          <ul className="job-description">{props.job_posts[0].qualifications.split('•').map((st,i)=> {
           if (st){
             return <li key={i}>{st}</li>
             }
