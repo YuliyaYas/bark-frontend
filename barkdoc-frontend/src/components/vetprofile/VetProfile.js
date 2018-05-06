b12d5b9e3fced798a8f58a8aa06561eb399d7a0d
Yuliya Yasenetska <jasenetska@gmail.com>
2018-05-06 12:06:38 -0400

    added reviews to vet prof

--- barkdoc-frontend/src/components/vetprofile/VetProfile.js
+++ barkdoc-frontend/src/components/vetprofile/VetProfile.js
@@ -20,6 +20,9 @@ componentDidMount(){
 }
 
   render(){
+    const currentVetReviews = (this.props.vets_reviews.length > 0 && this.state.vet.length > 0) ? this.props.vets_reviews.filter(v=> v.vet_name === this.state.vet[0].name) : '';
+    console.log("in vet pr", this.props.vets_reviews, "vet", this.state.vet);
+    console.log("reviews", currentVetReviews);
   return(
     <div>
       <div className="background-vet-background container-pattern-zig-zag">
@@ -35,8 +38,14 @@ componentDidMount(){
               </div>
               <div className="profile-info-r">
                 {this.state.vet.length > 0 ? <h1 className="white-font-profile bold content">{this.state.vet[0].name}</h1> : ""}
-                <h3 className="white-font-profile content">Veterinarian</h3>
-                <h3 className="white-font-profile content"><img alt="" className="stars float-left" src={require("../../img/5_stars.png")}/> Verified Patient Reviews(6)</h3>
+                {currentVetReviews.length > 0 ?
+                <div>
+                  <h3 className="white-font-profile content">Veterinarian</h3>
+                  <h3 className="white-font-profile content"><img alt="" className="stars float-left" src={require("../../img/5_stars.png")}/> Verified Patient Reviews({currentVetReviews.length})</h3>
+                </div>
+                :
+                ""
+              }
               </div>
             </div>
             <div className="services">
@@ -71,7 +80,7 @@ componentDidMount(){
               <p>
                 {this.state.vet.length > 0 ? this.state.vet[0].difference_question : "" }
                </p>
-              
+
               <hr />
               <h2 className="bold">Verified Patient Reviews (6)</h2>
               <h4><img alt="" className="stars float-left" src={require("../../img/5_stars.png")}/> 4.5 </h4>
@@ -92,80 +101,6 @@ componentDidMount(){
                   </div>
                 </div>
 
-                <div className="review">
-                  <div className="col-55">
-                    <div><span className="grey-font pt-12">Trish R. </span><span className="light-grey-font font-9 "> - Soho</span></div>
-                  </div>
-                  <div className="col-45">
-                    <img alt="" className="quotes quote-size" src={require("./../../img/repeated/quotations.png")} alt="" />
-                    <div className="review-text">
-                      <p>Dr. Ryan had a very open schedule which worked perfect for my schedule. So easy of a process!</p>
-                      <img alt="" className="stars float-left" src={require("../../img/5_stars.png")}/>
-                    </div>
-                    <br/>
-                    <br/>
-                  </div>
-                </div>
-
-                <div className="review">
-                  <div className="col-55">
-                    <div><span className="grey-font pt-12">Trish R. </span><span className="light-grey-font font-9 "> - Soho</span></div>
-                  </div>
-                  <div className="col-45">
-                    <img alt="" className="quotes quote-size" src={require("./../../img/repeated/quotations.png")} alt="" />
-                    <div className="review-text">
-                      <p>Dr. Ryan had a very open schedule which worked perfect for my schedule. So easy of a process!</p>
-                      <img alt="" className="stars float-left" src={require("../../img/5_stars.png")}/>
-                    </div>
-                    <br/>
-                    <br/>
-                  </div>
-                </div>
-
-                <div className="review">
-                  <div className="col-55">
-                    <div><span className="grey-font pt-12">Trish R. </span><span className="light-grey-font font-9 "> - Soho</span></div>
-                  </div>
-                  <div className="col-45">
-                    <img alt="" className="quotes quote-size" src={require("./../../img/repeated/quotations.png")} alt="" />
-                    <div className="review-text">
-                      <p>Dr. Ryan had a very open schedule which worked perfect for my schedule. So easy of a process!</p>
-                      <img alt="" className="stars float-left" src={require("../../img/5_stars.png")}/>
-                    </div>
-                    <br/>
-                    <br/>
-                  </div>
-                </div>
-
-                <div className="review">
-                  <div className="col-55">
-                    <div><span className="grey-font pt-12">Trish R. </span><span className="light-grey-font font-9 "> - Soho</span></div>
-                  </div>
-                  <div className="col-45">
-                    <img alt="" className="quotes quote-size" src={require("./../../img/repeated/quotations.png")} alt="" />
-                    <div className="review-text">
-                      <p>Dr. Ryan had a very open schedule which worked perfect for my schedule. So easy of a process!</p>
-                      <img alt="" className="stars float-left" src={require("../../img/5_stars.png")}/>
-                    </div>
-                    <br/>
-                    <br/>
-                  </div>
-                </div>
-
-                <div className="review">
-                  <div className="col-55">
-                    <div><span className="grey-font pt-12">Trish R. </span><span className="light-grey-font font-9 "> - Soho</span></div>
-                  </div>
-                  <div className="col-45">
-                    <img alt="" className="quotes quote-size" src={require("./../../img/repeated/quotations.png")} alt="" />
-                    <div className="review-text">
-                      <p>Dr. Ryan had a very open schedule which worked perfect for my schedule. So easy of a process!</p>
-                      <img alt="" className="stars float-left" src={require("../../img/5_stars.png")}/>
-                    </div>
-                    <br/>
-                    <br/>
-                  </div>
-                </div>
               </div>
               <button className="orange white-font bold book-now-btn">BOOK NOW</button>
           </div>
